7ccdfc30aaba373e8ddb6b7ca21d76675f5b1c1d
Indiana1808 <jenitatindiana@yahoo.com>
2025-04-18 19:52:39 +0200

    Importing and rendering the page content in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
+import Home from "./pages/Home/Home";
+import Contact from './pages/Contact/Contact';
+import About from './pages/About/About';
+import Project from './pages/Project/Project';
 
 export default function App() {
   return (
-    <div>App</div>
+    <>
+      <div>
+
+        <header>
+          <Home/>
+        </header>
+
+        <main>
+          <About/>
+          <Project/>
+        </main>
+
+        <footer>
+          <Contact/>
+        </footer>
+
+      </div>
+
+    </>
   )
 }
