876e88dd2884c66c6b773527292575153c44b4de
Indiana1808 <jenitatindiana@yahoo.com>
2025-04-18 21:21:44 +0200

    intsall sass-embedded

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import style from './About.module.scss'
 
 export default function About() {
     return (
         
-    <div>About</div>
+    <div className={style.container}>
+        About
+    </div>
 )
 }
