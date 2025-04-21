2575df75d20c30ba443e7e77920d63545de68f7e
Indiana1808 <jenitatindiana@yahoo.com>
2025-04-21 23:42:21 +0200

    Implementing navigation component

---
+++ src/components/Header/navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Navcard from './navcard/Navcard';
+import './Navcard.module.scss';
+
+const navItems = [
+  { label: '_hello', target: '/home' },  // Lien vers la page hello
+  { label: '_about-me', target: '/about' },  // Lien vers la page about
+  { label: '_projects', target: '/project' },  // Lien vers la page projects
+  { label: '_contact-me', target: '/contact' },  // Lien vers la page contact
+]
+
+export default function Navbar() {
+  return (
+    <div className='Container'>
+      {navItems.map((item, index) => (
+        <Navcard key={index} label={item.label} target={item.target} />
+        ))}
+    </div>
+  )
+}
