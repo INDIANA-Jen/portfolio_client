2febe64600c1d610accbea31db2cdc5b11c176fe
Indiana1808 <jenitatindiana@yahoo.com>
2025-04-22 00:14:00 +0200

    Update to the navbar navigation  development

--- src/components/Header/navbar/Navbar.jsx
+++ src/components/Header/navbar/Navbar.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import Navcard from './navcard/Navcard';
-import './Navcard.module.scss';
+//import './Navcard.module.scss';
 
 const navItems = [
-  { label: '_hello', target: '/home' },  // Lien vers la page hello
+  { label: '_hello', target: '/' },  // Lien vers la page hello
   { label: '_about-me', target: '/about' },  // Lien vers la page about
   { label: '_projects', target: '/project' },  // Lien vers la page projects
   { label: '_contact-me', target: '/contact' },  // Lien vers la page contact
