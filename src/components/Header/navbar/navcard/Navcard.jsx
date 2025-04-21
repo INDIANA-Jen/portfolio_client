8df64b7a6162d25212f34f82e996bbefc0e194b2
Indiana1808 <jenitatindiana@yahoo.com>
2025-04-22 00:10:10 +0200

    NavCard update: integration of React Router Link

--- src/components/Header/navbar/navcard/Navcard.jsx
+++ src/components/Header/navbar/navcard/Navcard.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import { Link } from 'react-scroll';
+import { Link } from 'react-router-dom';
 
 export default function Navcard({label, target}) {
   return (
     <div className='navcard'>
-      <Link href= {target}>
-        {label}
-      </Link>
+      <Link to={target}>{label}</Link>
     </div>
   )
 }
