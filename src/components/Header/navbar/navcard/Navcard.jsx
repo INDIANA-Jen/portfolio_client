82b0f042873c09b26c0779000708a79f19904b51
Indiana1808 <jenitatindiana@yahoo.com>
2025-04-23 21:54:59 +0200

    Comment and modify the className of Navcard

--- src/components/Header/navbar/navcard/Navcard.jsx
+++ src/components/Header/navbar/navcard/Navcard.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
+
+//Importing the Link component from react-router-dom for navigation without page reload.
 import { Link } from 'react-router-dom';
+
+// Declaration of the Navcard component, which takes label and target as props. 
+// label: the text that will be displayed in the link. 
+// target: the URL of the route to which the link should redirect.
 
 export default function Navcard({label, target}) {
   return (
-    <div className='navcard'>
+
+    // A div container with the class 'contenair', used to style this component.
+    <div className='contenair'>
+      {/* The Link component allows navigation to the route specified by target. */}
       <Link to={target}>{label}</Link>
     </div>
   )
 }
 
-//to verify and validate
 
