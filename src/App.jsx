c177025acdb4437a296c4e1ee5eec1d658989433
Indiana1808 <jenitatindiana@yahoo.com>
2025-04-23 21:42:56 +0200

    Comment the App code and also rename the style file from Base to Base.module.scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,21 @@
 import React from 'react'
+
+//Import the Route component.
 import Route from './Route'
 
+//Import modular App styles.
 import styles from './App.module.scss';
 
-import './styles/Base.scss'
+//Import modular Base styles 
+import './styles/Base.module.scss'
 
 export default function App() {
   return (
     <>
     <div className={styles.container}>
-      <Route/>
+      <Route/> {/* Include the entire routing system in this. */}
     </div>
         
     </>
   )
 }
-
-
-/*import React from 'react'
-import Home from "./pages/Home/Home";
-import Contact from './pages/Contact/Contact';
-import About from './pages/About/About';
-import Project from './pages/Project/Project';
-
-export default function App() {
-  return (
-    <>
-      <div>
-
-        <header>
-          <Home/>
-        </header>
-
-        <main>
-          <About/>
-          <Project/>
-        </main>
-
-        <footer>
-          <Contact/>
-        </footer>
-
-      </div>
-
-    </>
-  )
-}*/
