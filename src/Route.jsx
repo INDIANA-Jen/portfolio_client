abdc3992263fdc86b1b72e68bdae391829a71e49
Indiana1808 <jenitatindiana@yahoo.com>
2025-05-08 00:03:43 +0200

    update and test another code Route

--- src/Route.jsx
+++ src/Route.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+/*import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import Home from './pages/home/Home';
 import About from './pages/about/About';
 import Project from './pages/project/Project';
 import Contact from './pages/contact/Contact';
+
+
 
 
 const router = createBrowserRouter([
@@ -16,4 +18,40 @@ const router = createBrowserRouter([
 
 export default function AppRoutes() {
     return <RouterProvider router={router} />;
+}*/
+
+
+import React from 'react';
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+
+import Home from './pages/home/Home';
+import About from './pages/about/About';
+import Project from './pages/project/Project';
+import Contact from './pages/contact/Contact';
+import Navbar from './components/Navbar'; // Import de la Navbar
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      { path: '/', element: <Home /> },
+      { path: '/about', element: <About /> },
+      { path: '/project', element: <Project /> },
+      { path: '/contact', element: <Contact /> },
+    ]
+  }
+]);
+
+export default function AppRoutes() {
+  return <RouterProvider router={router} />;
 }
