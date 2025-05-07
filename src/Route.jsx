bf5d95464435ca6a1b0c13063ec6c125fb26ddaa
Indiana1808 <jenitatindiana@yahoo.com>
2025-05-07 23:09:38 +0200

    Rename the folder names of each pag

--- src/Route.jsx
+++ src/Route.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import Home from './Pages/home/Home';
-import About from './pages/About/About';
+import Home from './pages/home/Home';
+import About from './pages/about/About';
+import Project from './pages/project/Project';
+import Contact from './pages/contact/Contact';
+
 
 const router = createBrowserRouter([
     { path: "/", element: <Home /> },
-    { path: "/About", element: <About /> }
+    { path: "/About", element: <About /> },
+    { path: "/Project", element: <Project />},
+    { path: "/Contact", element: <Contact />}
 ]);
 
 export default function AppRoutes() {
