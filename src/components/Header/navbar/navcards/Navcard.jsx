546db2d91f7a57215848364d272cb624245a9590
Indiana1808 <jenitatindiana@yahoo.com>
2025-05-07 23:57:22 +0200

    update test code Navcard

--- src/components/Header/navbar/navcards/Navcard.jsx
+++ src/components/Header/navbar/navcards/Navcard.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 
 
 import { Link } from 'react-router-dom';
@@ -9,14 +9,28 @@ import styles from './Navcard.module.scss';
 export default function Navcard({label, target}) {
   return (
 
-   
     <div className={styles.container}>
       
       <Link className={styles.Link} to={target} data-label={label}>{label}</Link>
     </div>
   )
-}
- 
+}*/
 
+// components/navcards/Navcard.jsx
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import styles from './Navcard.module.scss';
+
+const Navcard = ({ label, target }) => {
+  return (
+    <div className={styles.navcard}>
+      <NavLink to={target} className={styles.navlink}>
+        {label}
+      </NavLink>
+    </div>
+  );
+};
+
+export default Navcard;
 
 
