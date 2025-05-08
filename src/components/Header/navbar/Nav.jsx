6b1baff02f6090669f471a1b1829acbaca3cad67
Indiana1808 <jenitatindiana@yahoo.com>
2025-05-08 11:28:23 +0200

    Removal of the old Nav code and proper structiring of the new code

--- src/components/Header/navbar/Nav.jsx
+++ src/components/Header/navbar/Nav.jsx
@@ -1,44 +1,13 @@
-/*import React from 'react';
-
-
-import styles from './Nav.module.scss';
-import Navcard from './navcards/Navcard';
-
-
-const navItems = [
-  {label : 'micheal-weaver', target: '/'},
-  { label: '_hello', target: '/' },  
-  { label: '_about-me', target: '/about' },  
-  { label: '_projects', target: '/project' },  
-  { label: '_contact-me', target: '/contact' },  
-]
-
-export default function Navbar() {
-  return (
-
-    
-    <div className={styles.container}>
-
-    
-      {navItems.map((item, index) => (
-
-       
-        <Navcard key={index} label={item.label} target={item.target} />
-        ))}
-    </div>
-  )
-}
-*/
-// components/Navbar.jsx
 import React from 'react';
 import Navcard from './navcards/Navcard';
 import styles from './Nav.module.scss';
 
 const navItems = [
-  { label: 'Accueil', target: '/' },
-  { label: 'À propos', target: '/about' },
-  { label: 'Projets', target: '/project' },
-  { label: 'Contact', target: '/contact' },
+  { label: 'micheal-weaver', target: '/' },
+  { label: '_hello', target: '/' },
+  { label: '_about-me', target: '/about' },
+  { label: '_projects', target: '/project' },
+  { label: '_contact-me', target: '/contact' },
 ];
 
 export default function Navbar() {
