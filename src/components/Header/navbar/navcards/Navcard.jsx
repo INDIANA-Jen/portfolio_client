1be86ec186a6f5daaec78b1ef29acaa988f2f16b
Indiana1808 <jenitatindiana@yahoo.com>
2025-05-08 12:01:17 +0200

    Code explanation in Navcard comments

--- src/components/Header/navbar/navcards/Navcard.jsx
+++ src/components/Header/navbar/navcards/Navcard.jsx
@@ -3,9 +3,15 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import styles from './Navcard.module.scss';
 
+{/* Declares a functional component called Navcard that takes two props : label (the text to display) and target  (the route to navigate to)  */}
 const Navcard = ({ label, target }) => {
   return (
     <div className={styles.navcard}>
+
+      {/* to={target}: defines the target route*/}
+      {/* className={styles.navlink: applies a locally scoped styled class */}
+      {/* {label}: dynamically displays the link text */}
+      
       <NavLink to={target} className={styles.navlink}>
         {label}
       </NavLink>
