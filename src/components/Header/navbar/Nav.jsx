0d875798ecb2edaa088b9fec85a8953f2824dc8d
Indiana1808 <jenitatindiana@yahoo.com>
2025-05-08 12:25:14 +0200

    Code explanation in Nav comments

--- src/components/Header/navbar/Nav.jsx
+++ src/components/Header/navbar/Nav.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import Navcard from './navcards/Navcard';
 import styles from './Nav.module.scss';
 
+{/* Definition of navigation items */}
+{/* This is an array of objetcts representing the links in the navigation menu : 
+    label : the text displayed on each link 
+    target: the route each link redirects to
+  */}
 const navItems = [
   { label: 'micheal-weaver', target: '/' },
   { label: '_hello', target: '/' },
@@ -10,7 +15,17 @@ const navItems = [
   { label: '_contact-me', target: '/contact' },
 ];
 
+
 export default function Navbar() {
+
+  {/* definition of navigation items: 
+    It loops through each element in navItemes using map(...) 
+    for eact item, it renders a Navcard components, passing:
+          label : the text displayed on each link or the link text
+          target : the route each link redirects to or the link destination
+    
+    the key helps React keep track of each element during  dynamic rendering (here index is used, but an id would be more robust if available)
+     */}
   return (
     <div className={styles.container}>
       {navItems.map((item, index) => (
