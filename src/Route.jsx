ce3b499e5453b0d47274268d816a66be345b1c88
Indiana1808 <jenitatindiana@yahoo.com>
2025-05-08 12:50:59 +0200

    Code explanation in Route comments

--- src/Route.jsx
+++ src/Route.jsx
@@ -1,6 +1,11 @@
 
 import React from 'react';
 import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+{/* Explanation of the route : 
+  createBrowserRouter : creates a router based on the browser's hitsory (like in a Single page application)
+  RouterProvider : makes the router available throughout the application
+  Outlet : dynamically renders the child components of a route 
+  */}
 
 import Home from './pages/home/Home';
 import About from './pages/about/About';
@@ -8,7 +13,10 @@ import Project from './pages/project/Project';
 import Contact from './pages/contact/Contact';
 import Navbar from './Components/Header/navbar/Nav';
 
-
+{/* Layout component: Displays the navigation bar (Navbar) 
+  then, using <Outlet />, it dynamically renders the page corresponding to the route 
+  it is a common layout for all child pages 
+  */}
 const Layout = () => {
   return (
     <>
@@ -18,6 +26,11 @@ const Layout = () => {
   );
 };
 
+{/* Defines a set of routes
+  The root (/) displays the layout component
+  then, depending on the path (/, /about, etc), the corresponding components is displayed in the Outlet of the Layout
+  this allows sharing the same structure (Navbar) while displaying different pages based to the route   
+  */}
 const router = createBrowserRouter([
 
   {
@@ -33,8 +46,21 @@ const router = createBrowserRouter([
   }
 ]);
 
+{/*
+  Main Component: AppRoutes, This component encapsulates the entire navigation system
+  it makes the router available to the entire application through RouterProvider
+   */}
 export default function AppRoutes() {
 
   return <RouterProvider router={router} />;
 
 }
+
+
+{/* Summary 
+  code steps up a clean navigation architecture in a React application, with: 
+  - A reusable Layout (Navbar + Outlet),
+  - Routes defined using creatBrowserRouter,
+  - And dynamic page rendering through the Outlet component
+  
+  */}
